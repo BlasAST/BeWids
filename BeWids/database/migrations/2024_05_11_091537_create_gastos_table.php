c773f8077eb70c7c4713f6679ca4fcff4ffa08d0
JulioLir <jliroladelaguila@educa.madrid.org>
2024-05-11 12:10:09 +0200

    migraciones gastos y deudas

---
+++ BeWids/database/migrations/2024_05_11_091537_create_gastos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gastos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_portal');
+            $table->string('titulo');
+            $table->string('tipo');
+            $table->double('cantidad');
+            $table->date('fecha');
+            $table->string('pagado_por');
+            $table->string('participantes');
+            $table->timestamps();
+
+            $table->foreign('id_portal')->references('id')->on('portales')->onDelete('cascade');
+            $table->foreign('pagado_por')->references('nombre_en_portal')->on('participantes')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gastos');
+    }
+};
