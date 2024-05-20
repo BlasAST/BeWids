74fdc6a1ccaa2b0c8d0f9e8b25966738c93e7043
JulioLir <jliroladelaguila@educa.madrid.org>
2024-05-20 20:27:31 +0200

    contabilidad

--- BeWids/app/Http/Controllers/Contabilidad.php
+++ BeWids/app/Http/Controllers/Contabilidad.php
@@ -8,6 +8,8 @@ use App\Models\Participantes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
+use function PHPUnit\Framework\isEmpty;
+
 class Contabilidad extends Controller
 {
     public function index(){
@@ -24,7 +26,8 @@ class Contabilidad extends Controller
         $gasto->pagado_por = request('pagador');
         $participantes = "";
         if(request('participantes')){
-            $parte = request('cantidad') / count(request('participantes'));
+            $parte = round(request('cantidad') / count(request('participantes')),2);
+            $dif = request('cantidad') - $parte * count(request('participantes'));
             foreach(request('participantes') as $participante){
                 $participantes .= $participante.";";
                 $persona = Participantes::where('id_portal',Session::get('portal')->id)->where('nombre_en_portal',$participante)->first();
@@ -36,7 +39,7 @@ class Contabilidad extends Controller
         $gasto->save();
 
         $pagador= Participantes::where('id_portal',Session::get('portal')->id)->where('nombre_en_portal',request('pagador'))->first();
-        $pagador->deuda += request('cantidad');
+        $pagador->deuda += request('cantidad') - $dif;
         $pagador->save();
         $participantes = Participantes::where('id_portal',Session::get('portal')->id)->get();
         Session::put('participantes',$participantes);
@@ -44,18 +47,168 @@ class Contabilidad extends Controller
         return redirect()->to('/contabilidad');
     }
 
-    public function hacerCuentas(){
-        $$deudas = Participantes::selectRaw('nombre_en_portal, SUM(cantidad) as total')
-                ->where('id_portal', Session::get('portal')->id)
-                ->groupBy('nombre_en_portal')
-                ->get();
-        foreach($deudas as $deuda){
-            if($deuda->total > 0)
-                $pagar[$deuda->participante] = $deuda->total;
-            if($deuda->total <0)
-                $recibir[$deuda->participante] = $deuda->total;
+    private function hacerCuentas(){
+        $participantes = Participantes::where('id_portal', Session::get('portal')->id)->get();
+        foreach($participantes as $participante){
+            if($participante->deuda < 0)
+                $pagadores[$participante->nombre_en_portal] = round(abs($participante->deuda),2);
+            if($participante->deuda > 0)
+                $receptores[$participante->nombre_en_portal] = round($participante->deuda,2);
         }
 
-        return ($deudas);
+        $min = PHP_INT_MAX;
+        $minTransacciones = [];
+        $combP = $this->crearCombinaciones($pagadores);
+        $combR = $this->crearCombinaciones($receptores);
+        $transacciones = [];
+
+
+        foreach($combP as $pagar){
+            foreach($combR as $recibir){
+                $transacciones[] = $this->generarTransacciones($pagar,$recibir);
+                // echo "<pre>";
+                // var_dump($transacciones);
+                // echo "</pre>";
+                
+                
+            }
+        }
+        foreach($transacciones as $caso){
+            switch(true){
+                case count($caso) < $min:
+                    $minTransacciones = [];
+                    $minTransacciones[] = $caso;
+                    $min = count($caso);
+                    break;
+                case count($caso) == $min:
+                    // if(round(rand(0,1))){
+                    //     $minTransacciones = [];
+                    // }
+                    $minTransacciones[] = $transacciones;
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
+        do{
+            foreach($minTransacciones as $key => $opcion){
+                if(round(rand(0,1)) && count($minTransacciones) > 1){
+                   unset($minTransacciones[$key]);
+                } 
+            }
+        }while(count($minTransacciones) == 1);
+        echo "<pre>";
+        var_dump($minTransacciones);
+        echo "</pre>";
+
+
+    }
+
+    private function generarTransacciones2($pagar, $recibir, $transacciones){
+
+
+
+        do{
+            
+
+        }while(true);
+
+
+
+
+
+        // $pagadores = $this->crearCombinaciones($pagar);
+        // $receptores = $this->crearCombinaciones($recibir);
+        // $transaccionesTotales = [];
+        // foreach($pagadores as $deudores){
+        //     foreach($receptores as $acreditadores){
+        //         $transaccionesTotales[] = $this->generarTransacciones2($deudores, $acreditadores, $transacciones);
+        //     }
+        // }
+
+        
+        
+    }
+    private function unaTrans($pagar,$recibir,$transacciones){
+        foreach ($recibir as $receptor => $cantidad){
+            $pagador = array_search($cantidad,$pagar);
+            if($pagador){
+                $transacciones[] = ["deudor" => $pagador,"receptor"=>$receptor,'cantidad'=>$cantidad];
+                $pagar[$pagador] -= $cantidad;
+                unset($recibir[$receptor]);
+                unset($pagar[$pagador]);
+            }
+        };
+        foreach($recibir as $receptor => $cantidad){
+            $pagador = array_key_first($pagar);
+            if ($pagador === null) break;
+            if($pagar[$pagador] > $cantidad){
+                $transacciones[] = ["deudor" => $pagador,"receptor"=>$receptor,'cantidad'=>$cantidad];
+                $pagar[$pagador] -= $cantidad;
+            }else{
+                $transacciones[] = ["deudor" => $pagador,"receptor"=>$receptor,'cantidad'=>$pagar[$pagador]];
+                $recibir[$receptor] -= $pagar[$pagador];
+                unset($pagar[$pagador]);
+            }
+        }
+        if(isEmpty($pagar)||isEmpty($recibir)){
+
+        }
+    }
+    private function generarTransacciones($pagar,$recibir){
+        foreach ($pagar as $pagador => $deuda) {
+            foreach ($recibir as $receptor => $cantidad) {
+                $graph[$pagador][$receptor] = 0;
+            }
+        }
+        
+        // Llenar la matriz de deudas
+        foreach ($pagar as $pagador => $deuda) {
+            foreach ($recibir as $receptor => $cantidad) {
+                    if ($pagar[$pagador] > $recibir[$receptor]) {
+                        $graph[$pagador][$receptor] = $recibir[$receptor];
+                        $pagar[$pagador] = round($pagar[$pagador] - $recibir[$receptor],2);
+                        $recibir[$receptor] = 0;
+                    } else {
+                        $graph[$pagador][$receptor] = $pagar[$pagador];
+                        $recibir[$receptor] = round($recibir[$receptor] - $pagar[$pagador],2);
+                        $pagar[$pagador] = 0;
+                        break;
+                    }   
+            }
+        }
+        // Generar las transacciones
+        foreach ($graph as $pagador => $recibir) {
+            foreach ($recibir as $receptor => $deuda) {
+                if ($deuda > 0) {
+                    $result[] = ["deuda"=>$deuda,"pagador"=>$pagador,"receptor"=>$receptor];
+                }
+            }
+        }
+        return $result;
+    }
+    private function crearCombinaciones($array) {
+        
+        // Si el array tiene un solo elemento, devolverlo como única permutación
+        if (count($array) === 1) {
+            return [$array];
+        }
+        
+        // Iterar sobre cada elemento del array
+        foreach ($array as $key => $value) {
+            // Eliminar el elemento actual y obtener el resto del array
+            $subArray = array_diff_key($array, [$key => $value]);
+            
+            // Obtener las permutaciones del resto del array
+            $combinaciones = $this->crearCombinaciones($subArray);
+            
+            // Añadir el elemento actual a cada una de las permutaciones obtenidas
+            foreach ($combinaciones as $combinacion) {
+                $arrayCombinado[] = [$key => $value] + $combinacion;
+            }
+        }
+        
+        return $arrayCombinado;
     }
 }
