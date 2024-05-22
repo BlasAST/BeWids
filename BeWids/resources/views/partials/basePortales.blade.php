d0c5b178c0365b80591e7d4a1720965763b5d879
BlasAST <blas.siguencia@educa.madrid.org>
2024-05-22 22:54:13 +0200

    Tailwind configuracion completada

--- BeWids/resources/views/partials/basePortales.blade.php
+++ BeWids/resources/views/partials/basePortales.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    @vite('resources/css/app.css')
+    <!-- @vite('resources/css/app.css') -->
+    @vite('public/css/tailwindBase.css')
 </head>
 <body class="bg-colorFondo w-screen h-screen">
-<img src="{{asset('imagenes/imagenesBase/imagenFondo.png')}}" class="w-screen h-screen">
+<img src="{{asset('imagenes/imagenesBase/imagenFondo2.png')}}" >
+<div class="lechugaOavion">BUeeenas</div>
+<div class="bg-slate-600">asefa</div>
 </body>
 </html>
