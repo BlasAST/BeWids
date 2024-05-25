f1e08ab16181643f537b6acf402712155464a5cc
BlasAST <blas.siguencia@educa.madrid.org>
2024-05-25 20:26:49 +0200

    Organizacion servicio Chat

--- BeWids/resources/views/livewire/chat/lista-chats.blade.php
+++ BeWids/resources/views/livewire/chat/lista-chats.blade.php
@@ -1,40 +1,22 @@
-<div>
-    <div class="lista_header">
-        <p>Chat</p>
-        <img src="https://picsum.photos/200" alt="">
-    </div>
-    
-    <div class="lista_main">
-        @php
-            $portal=Session::get('portal');
-        @endphp
-        <br>
-        <div><input type="text" placeholder="Buscar Participante"></div>
-        <div>
-            <h1>{{$portal->id}}</h1>
-            <h3>Mensajes:</h3>
-            <br>
-            <div class="chatItem">
-                <img src="https://picsum.photos/200" alt="">
-                <div class="infoUser">
-                    <div class="itemsSup">
-                        <div class="nombreChat">Ejemplo</div>
-                        <div class="fecha">3h</div>
-                    </div>
-                    <div class="notificacion"></div>
-                    <div class="resumen">Deja de llamarme...</div>
-                    <div class="sinLeer"><p>52</p></div>
-                </div>
-            </div>
+<div class="lista bg-colorBarra2 basis-1/4 flex flex-col">
+    <header class="flex border-b-2 border-b-blue-600 justify-between items-center h-1/6 mx-4">
+        <select type="text" value="Buscar Participante" class="rounded-3xl">
+            <option value="">Buscar participante</option>
             @foreach($participantes as $participante)
-                    <li>{{$participante->nombre_en_portal}}</li>
+            <option value="{{$participante}}">{{$participante->nombre_en_portal}}</option>
             @endforeach
-
-            <br><br><br>
-            <button>Crear nuevo chat</button>
-            <ul>
-
-            </ul>
-        </div>
-    </div>
+        </select>
+    </header>
+    <main class="seleccionesChat flex flex-col h-full justify-around items-center">
+        <h2 class="flex items-center">
+            <span class="relative flex h-3 w-3 mr-2">
+                <span class="animate-ping absolute h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                <span class="rounded-full h-3 w-3 bg-sky-500"></span>
+            </span>
+            Chat global
+        </h2>
+        <h2>Destacados</h2>
+        <h2>Bandeja de entrada</h2>
+        <h2>Grupos internos</h2>
+    </main>
 </div>
--- BeWids/resources/views/vistas2/chatYEncuestas.blade.php
+++ BeWids/resources/views/vistas2/chatYEncuestas.blade.php
@@ -1,22 +1,35 @@
-@extends('partials.base')
-@section('rutaEstilos','../css/estilosChatYEncuestas.css')
-@section('rutaEstilos2','../css/estilosBaseServicios.css')
+@extends('partials/plantillaServicios')
+@section('rutaJs','js/chatYEncuestas.js')
+@section('categorias')
+@php 
+    $portal=Session::get('portal')
+@endphp
 
-@section('rutaJs','../js/chatYEncuestas.js')
-@section('rutaJs2','../js/basicServicios.js')
-@section('contenido')
-@include('partials.header') 
-<main>
-    <div class="categorias">
-        <span id="chat">
-            <p class="selected">Chat</p>
-        </span>
-        <span id="encuestas">
-            <p>Encuestas</p>
-        </span>
-    </div>
-    @livewire('chat.mainn')
-     
-</main>
+<div id="chatCat" class="flex-grow flex justify-center cursor-pointer hover:text-colorDetalles">
+    <span class="h-full flex flex-col justify-center border-b-4 border-white  selected">Chat</span>
+</div>
+<div id= "encuestasCat"class="flex-grow flex justify-center cursor-pointer  hover:text-colorDetalles ">
+    <span class="h-full flex flex-col justify-center">Encuestas</span>
+</div>
+
+@endsection
+
+
+
+@section('contenidoServicio')
+
+<section id="chat" class="mostrar flex h-full w-full">
+    @livewire('chat.lista-chats')
+    @livewire('chat.contenedor-mensajes')
+</section>
+
+<section id="encuestas" class="hidden">
+
+
+</section>
+
+
+
+
 
 @endsection
