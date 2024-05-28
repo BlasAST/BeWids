7860d27067b0e757c3e98a1a1f218bc414115b1c
BlasAST <blas.siguencia@educa.madrid.org>
2024-05-29 00:53:50 +0200

    Inicio de Encuestas

---
+++ BeWids/app/Livewire/Encuestas/Encuestas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Encuestas;
+
+use Livewire\Component;
+
+class Encuestas extends Component
+{
+    public function render()
+    {
+        return view('livewire.encuestas.encuestas');
+    }
+}
---
+++ BeWids/resources/views/livewire/encuestas/encuestas.blade.php
@@ -0,0 +1,144 @@
+<div class="encuestas w-full h-full">
+    <div class="botonesEncuestas flex justify-center">
+    <button class="bg-colorBarra2 text-white p-4 rounded-xl mx-4">Crear encuesta</button>
+    <button class="bg-colorBarra2 text-white p-4 rounded-xl mx-4">Encuestas finalizadas</button>
+    </div>
+    
+    <div class="listadoEncuestas w-full h-full border-4 overflow-y-scroll">
+        <table class="">
+            <thead class="border-4 border-solid  border-red-700 sticky top-0 bg-blue-600">
+                <tr>
+                    <th>Nombre encuesta</th>
+                    <th>Porcetaje positivo</th>
+                    <th>Porcentaje negativo</th>
+                    <th>Votadores</th>
+                    <th>Descripción</th>
+                    <th><button>Detalles</button></th>
+                    <th><button>Votar</button></th>
+                </tr>
+            </thead>
+            <tbody class="border-4 border-solid border-blue-700 ">
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr><tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr> <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+                <tr>
+                    <td>Valor1</td>
+                    <td>Valor2</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+</div>
