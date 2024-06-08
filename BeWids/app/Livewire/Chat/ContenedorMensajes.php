da85a46acb9ec33b7bd9f217ccf801e5e0d6779f
BlasAST <blas.siguencia@educa.madrid.org>
2024-06-09 01:18:58 +0200

    Funcionalidad casi completa de chat

--- BeWids/app/Livewire/Chat/ContenedorMensajes.php
+++ BeWids/app/Livewire/Chat/ContenedorMensajes.php
@@ -18,16 +18,13 @@ class ContenedorMensajes extends Component
     public $participanteActual;
     public $participanteSeleccionado;
     public $participantesSeleccionados;
-    public $prueba='Hola buenas tardes';
-    // public $arrayParticipantes = [];
     public $mensajes;
 
     #[On('newChatSimple')]
     public function cargarConversacionIndivual(Conversacion $conversacion, Participantes $participante)
     {
-        $this->inforParticipante=NULL;
+        $this->inforParticipante = NULL;
         $this->participantesSeleccionados = False;
-        // $this->arrayParticipantes = [];
         $this->participanteActual = Participantes::where('id_usuario', auth()->user()->id)->where('id_portal', FacadesSession::get('portal')->id)->first();
         $this->conversacionSeleccionada = $conversacion;
         $this->participanteSeleccionado = $participante;
@@ -37,59 +34,91 @@ class ContenedorMensajes extends Component
     #[On('newChatGroup')]
     public function cargarConversacionGrupal(Conversacion $conversacion, $participantes)
     {
-        $this->inforParticipante=NULL;
+        $this->inforParticipante = NULL;
         $this->participanteSeleccionado = False;
         $this->participanteActual = Participantes::where('id_usuario', auth()->user()->id)->where('id_portal', FacadesSession::get('portal')->id)->first();
         $this->participantesSeleccionados = $participantes;
         $this->conversacionSeleccionada = $conversacion;
-        // foreach ($participantes as $participe) {
-        //     $this->arrayParticipantes[] = Participantes::where('id_portal', FacadesSession::get('portal')->id)
-        //         ->where('nombre_en_portal', $participe)->get();
-        // };
 
         $this->buscarMensajes();
     }
-
+    public $mensajesParticipanteActual;
+    public $mensajesParticipantesExternos;
+    public $prueba;
     public function buscarMensajes()
     {
+        $this->mensajes=NULL;
         if ($this->participanteSeleccionado) {
-            $this->mensajes = Mensaje::where('id_portal', FacadesSession::get('portal')->id)->where('conversacion_id',$this->conversacionSeleccionada->id)->where('emisor', $this->participanteActual->nombre_en_portal)->where('receptor', $this->participanteSeleccionado->nombre_en_portal)
-                ->orWhere('id_portal', FacadesSession::get('portal')->id)->where('conversacion_id',$this->conversacionSeleccionada->id)->where('emisor', $this->participanteSeleccionado->nombre_en_portal)->where('receptor', $this->participanteActual->nombre_en_portal)->get();
-            if ($this->mensajes->isEmpty()) {
-                $mensajesConversacion= new Mensaje();
-                $mensajesConversacion->id_portal=FacadesSession::get('portal')->id;
-                $mensajesConversacion->emisor=$this->participanteActual->nombre_en_portal;
-                $mensajesConversacion->receptor=$this->participanteSeleccionado->nombre_en_portal;
-                $mensajesConversacion->conversacion_id=$this->conversacionSeleccionada->id;
+            $this->mensajes = Mensaje::where('id_portal', FacadesSession::get('portal')->id)->where('conversacion_id', $this->conversacionSeleccionada->id)->where('emisor', $this->participanteActual->nombre_en_portal)->where('receptor', $this->participanteSeleccionado->nombre_en_portal)
+                ->orWhere('id_portal', FacadesSession::get('portal')->id)->where('conversacion_id', $this->conversacionSeleccionada->id)->where('emisor', $this->participanteSeleccionado->nombre_en_portal)->where('receptor', $this->participanteActual->nombre_en_portal)->first();
+
+            if ($this->mensajes == null) {
+                $mensajesConversacion = new Mensaje();
+                $mensajesConversacion->id_portal = FacadesSession::get('portal')->id;
+                $mensajesConversacion->emisor = $this->participanteActual->nombre_en_portal;
+                $mensajesConversacion->receptor = $this->participanteSeleccionado->nombre_en_portal;
+                $mensajesConversacion->conversacion_id = $this->conversacionSeleccionada->id;
                 $mensajesConversacion->save();
+                $this->mensajes = $mensajesConversacion;
             }
         } else {
-            $this->mensajes=Mensaje::where('id_portal',FacadesSession::get('portal')->id)->where('conversacion_id',$this->conversacionSeleccionada->id)->where('participantesGroup',json_encode($this->participantesSeleccionados))->get();
-            if($this->mensajes->isEmpty()){
-                $mensajesConversacion=new Mensaje();
-                $mensajesConversacion->id_portal=FacadesSession::get('portal')->id;    
-                $mensajesConversacion->conversacion_id=$this->conversacionSeleccionada->id;
-                $mensajesConversacion->participantesGroup=json_encode($this->participantesSeleccionados);
-                $mensajesConversacion->save();           
+            $this->mensajes = Mensaje::where('id_portal', FacadesSession::get('portal')->id)->where('conversacion_id', $this->conversacionSeleccionada->id)->where('participantesGroup','LIKE','%"'.$this->participanteActual->nombre_en_portal.'"%')->first();
+            // whereRaw("JSON_CONTAINS(participantesGroup,?)",['{"emisor": "' .$this->participantesSeleccionados.'"}'])->first();
+            // where('participantesGroup', json_encode($this->participantesSeleccionados))->first();
+            if ($this->mensajes == null) {
+                $mensajesConversacion = new Mensaje();
+                $mensajesConversacion->id_portal = FacadesSession::get('portal')->id;
+                $mensajesConversacion->conversacion_id = $this->conversacionSeleccionada->id;
+                $mensajesConversacion->participantesGroup = json_encode($this->participantesSeleccionados);
+                $mensajesConversacion->save();
+                $this->mensajes = $mensajesConversacion;
             }
         }
     }
 
     public $inforParticipante;
 
-    public function buscarInfoParticipantes($participe){
-        $this->participanteBuscado=Participantes::where('id_portal',FacadesSession::get('portal')->id)->where('nombre_en_portal',$participe)->first();
-        $this->inforParticipante=infousuario::where('id',$this->participanteBuscado->id_usuario)->first();
+    public function buscarInfoParticipantes($participe)
+    {
+        $this->participanteBuscado = Participantes::where('id_portal', FacadesSession::get('portal')->id)->where('nombre_en_portal', $participe)->first();
+        $this->inforParticipante = infousuario::where('id', $this->participanteBuscado->id_usuario)->first();
     }
 
-    public function cerrarConversacion(){
-        $this->participanteSeleccionado=NULL;
-        $this->participantesSeleccionados=NULL;
-        $this->inforParticipante=NULL;
+    public function cerrarConversacion()
+    {
+        $this->participanteSeleccionado = NULL;
+        $this->participantesSeleccionados = NULL;
+        $this->inforParticipante = NULL;
     }
-    public function cerrarInfo(){
-        $this->inforParticipante=NULL;
+    public function cerrarInfo()
+    {
+        $this->inforParticipante = NULL;
     }
+
+    public $mensajeEnviado;
+    public function enviarMensaje()
+    {
+        if ($this->mensajeEnviado != null) {
+            $enviando = [
+                'emisor' => $this->participanteActual->nombre_en_portal,
+                'mensaje' => $this->mensajeEnviado
+            ];
+
+            if ($this->mensajes->body == NULL) {
+                $updateBody = [];
+            } else {
+                $updateBody = json_decode($this->mensajes->body, true);
+            }
+            $updateBody[] = $enviando;
+
+            $updateBody = json_encode($updateBody);
+
+            $this->mensajes->update(['body' => $updateBody]);
+            $this->mensajeEnviado = NULL;
+        }
+    }
+
+
 
     public function render()
     {
--- BeWids/resources/views/livewire/chat/contenedor-mensajes.blade.php
+++ BeWids/resources/views/livewire/chat/contenedor-mensajes.blade.php
@@ -1,123 +1,154 @@
 <div class="contentedorMensajes grow flex flex-col">
     @if ($participanteSeleccionado)
-    <header class="flex basis-1 items-center relative">
-        <figure class=" w-8 mr-auto ml-4"><img src="https://picsum.photos/200" class="rounded-full" alt=""></figure>
-        <h1 class="mr-auto font-bold">{{$participanteSeleccionado->nombre_en_portal}}</h1>
-        <button class="mostrarListaParticipantes flex items-center" wire:click="$dispatch('toggleParticipantesList')">
-            <p class="mr-4 font-extralight">Menu</p>
-            <figure class="w-5 mr-4 flecha">
-                    <img src="{{asset('imagenes/imagenesBasic/flechaAperturaInfo.png')}}">
-                </figure>
-        </button>
-
-        <div class="bg-colorMain  participantesList hidden absolute top-7 right-0 p-4 flex flex-col ">
-            <button wire:click="buscarInfoParticipantes('{{$participanteSeleccionado->nombre_en_portal}}')" class="hover:bg-colorFondo"><p>Mostrar información</p></button>
-            <button wire:click="cerrarConversacion" class="hover:bg-colorFondo"> Cerrar conversación</button>
-           
-        </div>
-        @if ($inforParticipante)
-        <div class="bg-colorMain absolute top-12 right-5 w-[50%] p-10 mostrarInfo" >
-            <figure wire:click="cerrarInfo"><img src="{{asset('imagenes/imagenesBasic/cancel.svg')}}" alt=""></figure>
-            <h4><span class="text-blue-700">Nombre de usuario:</span><span>{{$inforParticipante->nombre}}</span></h4>
-            <h4><span class="text-blue-700">Nombre en el portal:</span><span>{{$participanteSeleccionado->nombre_en_portal}}</span></h4>
-            <h4><span class="text-blue-700">Descripción:</span><span>{{$inforParticipante->descripcion}}</span></h4>
-            <h4><span class="text-blue-700">Numero de contacto:</span><span>{{$inforParticipante->numero_contacto}}</span></h4>
-            <h4><span class="text-blue-700">Provincia:</span><span>{{$inforParticipante->provincia}}</span></h4>
-            <h4><span class="text-blue-700"></span><span></span>
-            
-        </div>
-        @endif
-        
-        
-        
-    </header>
-    @endif
-    @if($participantesSeleccionados)
-    <header class="flex basis-1 items-center relative justify-between px-5">
-        <h1 class="font-bold">{{$conversacionSeleccionada->name_group}}</h1>
-        <button class="mostrarListaParticipantes flex items-center" wire:click="$dispatch('toggleParticipantesList')">
-            <p class="mr-4 font-extralight">Menu</p>
-            <figure class="w-5 mr-4 flecha">
-                    <img src="{{asset('imagenes/imagenesBasic/flechaAperturaInfo.png')}}">
+        <header class="flex basis-1 items-center relative">
+            <figure class=" w-8 mr-auto ml-4"><img src="https://picsum.photos/200" class="rounded-full" alt="">
             </figure>
-        </button>
-        
-        <ul class="hidden participantesList absolute right-0 top-6 p-4 bg-colorMain flex flex-col justify-center" >
-            @foreach ($participantesSeleccionados as $participantee)
-                <li class="flex justify-center items-center hover:bg-colorFondo" wire:click="buscarInfoParticipantes('{{$participantee}}')">
-                    <p class="w-[50%]">{{$participantee}}</p>
-                    <figure class="w-5 mr-4 flecha" >
-                        <img src="{{asset('imagenes/imagenesBasic/flechaAperturaInfo.png')}}">
-                    </figure>
-                </li>
-            @endforeach
-            <button wire:click="cerrarConversacion" class="hover:bg-colorFondo">Cerrar conversación</button>
-        </ul>
-        @if ($inforParticipante)
-            <div class="bg-colorMain absolute top-12 right-5 w-[50%] p-10  mostrarInfo">
-                <figure wire:click="cerrarInfo"><img src="{{asset('imagenes/imagenesBasic/cancel.svg')}}" alt=""></figure>
-                <h4><span class="text-blue-700">Nombre de usuario:</span><span>{{$inforParticipante->nombre}}</span></h4>
-                <h4><span class="text-blue-700">Descripción:</span><span>{{$inforParticipante->descripcion}}</span></h4>
-                <h4><span class="text-blue-700">Numero de contacto:</span><span>{{$inforParticipante->numero_contacto}}</span></h4>
-                <h4><span class="text-blue-700">Provincia:</span><span>{{$inforParticipante->provincia}}</span></h4>
-                <h4><span class="text-blue-700"></span><span></span></h4>
-                    
+            <h1 class="mr-auto font-bold">{{ $participanteSeleccionado->nombre_en_portal }}</h1>
+            <button class="mostrarListaParticipantes flex items-center" wire:click="$dispatch('toggleParticipantesList')">
+                <p class="mr-4 font-extralight">Menu</p>
+                <figure class="w-5 mr-4 flecha">
+                    <img src="{{ asset('imagenes/imagenesBasic/flechaAperturaInfo.png') }}">
+                </figure>
+            </button>
+
+            <div class="bg-colorMain  participantesList hidden absolute top-7 right-0 p-4 flex flex-col ">
+                <button wire:click="buscarInfoParticipantes('{{ $participanteSeleccionado->nombre_en_portal }}')"
+                    class="hover:bg-colorFondo">
+                    <p>Mostrar información</p>
+                </button>
+                <button wire:click="cerrarConversacion" class="hover:bg-colorFondo"> Cerrar conversación</button>
+
             </div>
-        @endif
-            
+            @if ($inforParticipante)
+                <div class="bg-colorMain absolute top-12 right-[30%] w-[50%] p-10 mostrarInfo">
+                    <figure wire:click="cerrarInfo"><img src="{{ asset('imagenes/imagenesBasic/cancel.svg') }}"
+                            alt=""></figure>
+                    <h4><span class="text-blue-700">Nombre de
+                            usuario:</span><span>{{ $inforParticipante->nombre }}</span></h4>
+                    <h4><span class="text-blue-700">Nombre en el
+                            portal:</span><span>{{ $participanteSeleccionado->nombre_en_portal }}</span></h4>
+                    <h4><span
+                            class="text-blue-700">Descripción:</span><span>{{ $inforParticipante->descripcion }}</span>
+                    </h4>
+                    <h4><span class="text-blue-700">Numero de
+                            contacto:</span><span>{{ $inforParticipante->numero_contacto }}</span></h4>
+                    <h4><span class="text-blue-700">Provincia:</span><span>{{ $inforParticipante->provincia }}</span>
+                    </h4>
+                    <h4><span class="text-blue-700"></span><span></span>
 
- 
-    </header>
+                </div>
+            @endif
+
+
+
+        </header>
     @endif
-    <main class="containerMessages grow bg-slate-800 overflow-y-scroll flex flex-col">
-        @if ($participanteSeleccionado || $participantesSeleccionados)
-            
-        
-        <div class="other bg-white w-[60%] ml-5 rounded-lg my-3 p-3">
-            <p>Nombre</p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt, animi velit ullam incidunt rerum laudantium voluptatem, fugiat odio repellat saepe voluptatibus corrupti vero totam aut quos quibusdam esse! Exercitationem, molestias.
-            Inventore, enim unde? Magnam libero, nemo modi excepturi dolorem veniam recusandae molestiae quod provident quae totam fuga praesentium facere sed numquam ex eveniet doloribus alias saepe voluptas laudantium minus nihil.
-            Ipsa accusamus inventore maxime blanditiis. Excepturi id dolores nihil corporis quo amet quam distinctio repudiandae culpa. Laborum delectus accusamus alias consequatur amet minus maiores rem eos, sunt porro, modi iusto!
-            <p>Hace 5 horas</p>
-        </div>
-        <h1>HOla buenas tardes</h1>
-           
-        <h2>Funcion hijo de puta</h2>
-        
-        <div class="you w-[60%] self-end bg-blue-600 text-white mr-5 rounded-lg my-3 p-3 ">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate voluptatem quae porro. Commodi id est animi temporibus vel? Tempore quaerat nostrum sit facere voluptatum numquam sapiente commodi esse repellendus labore?
-            <p>Hace 11min</p>
-        </div>
-        @else
+    @if ($participantesSeleccionados)
+        <header class="flex basis-1 items-center relative justify-between px-5">
+            <h1 class="font-bold">{{ $conversacionSeleccionada->name_group }}</h1>
+            <button class="mostrarListaParticipantes flex items-center"
+                wire:click="$dispatch('toggleParticipantesList')">
+                <p class="mr-4 font-extralight">Menu</p>
+                <figure class="w-5 mr-4 flecha">
+                    <img src="{{ asset('imagenes/imagenesBasic/flechaAperturaInfo.png') }}">
+                </figure>
+            </button>
 
-        <h1 class="mt-8 text-center text-2xl text-white" >No hay ninguna conversación seleccionada</h1>
-        @if ($inforParticipante)
-            {{$inforParticipante=NULL}}
-        @endif
-        @endif
-    </main>
-    <footer class="sendMessage bg-blue-800 h-[10%] rounded-br-2xl flex">
-        <input type="text" placeholder="Escribir mensaje" class="grow placeholder:pl-4 focus:bg-colorFondo">
-        <button class="basis-1/6">Enviar</button>
-    </footer>
-    
+            <ul class="hidden participantesList absolute right-0 top-6 p-2 bg-colorMain flex flex-col justify-center">
+                @foreach ($participantesSeleccionados as $participantee)
+                    <li class="flex justify-center items-center hover:bg-colorFondo"
+                        wire:click="buscarInfoParticipantes('{{ $participantee }}')">
+                        <p class="w-[50%]">{{ $participantee }}</p>
+                        <figure class="w-5 mr-4 flecha">
+                            <img src="{{ asset('imagenes/imagenesBasic/flechaAperturaInfo.png') }}">
+                        </figure>
+                    </li>
+                @endforeach
+                <button wire:click="cerrarConversacion" class="hover:bg-colorFondo">Cerrar conversación</button>
+            </ul>
+            @if ($inforParticipante)
+                <div class="bg-colorMain absolute top-12 right-[42%] w-[50%] p-10  mostrarInfo">
+                    <figure wire:click="cerrarInfo"><img src="{{ asset('imagenes/imagenesBasic/cancel.svg') }}"
+                            alt=""></figure>
+                    <h4><span class="text-blue-700">Nombre de
+                            usuario:</span><span>{{ $inforParticipante->nombre }}</span></h4>
+                    <h4><span
+                            class="text-blue-700">Descripción:</span><span>{{ $inforParticipante->descripcion }}</span>
+                    </h4>
+                    <h4><span class="text-blue-700">Numero de
+                            contacto:</span><span>{{ $inforParticipante->numero_contacto }}</span></h4>
+                    <h4><span class="text-blue-700">Provincia:</span><span>{{ $inforParticipante->provincia }}</span>
+                    </h4>
+                    <h4><span class="text-blue-700"></span><span></span></h4>
+
+                </div>
+            @endif
+
+
+        </header>
+    @endif
+    @if ($participanteSeleccionado || $participantesSeleccionados)
+        <main class="containerMessages grow bg-slate-800 overflow-y-scroll flex flex-col">
+
+            @if ($prueba)
+                <h1>Aqui tamo</h1>
+                @foreach (json_decode($prueba) as $prue)
+                    {{ $prue->emisor }}
+                @endforeach
+            @endif
+
+            @if ($mensajes->body != null)
+                @foreach (json_decode($mensajes->body) as $mensaje)
+                    @if ($mensaje->emisor != $participanteActual->nombre_en_portal)
+                        <div class="other bg-white w-[20rem] ml-5 rounded-lg my-3 p-3">
+                            <p>{{$mensaje->emisor}}</p>
+                            <div>{{$mensaje->mensaje}}</div>
+                            <p>Hace 5 horas</p>
+                        </div>
+                    @endif
+
+
+                    @if ($mensaje->emisor == $participanteActual->nombre_en_portal)
+                        <div class="you w-[20rem] self-end bg-blue-600 text-white mr-5 rounded-lg my-3 p-3 ">
+                            <p class="text-right">Tu</p>
+                            <div class="break-words whitespace-normal">{{$mensaje->mensaje}}</div>
+                            <p>Hace 11min</p>
+                        </div>
+                    @endif
+                @endforeach
+            @else
+            <h1 class="mt-8 text-center text-2xl text-white">Nueva conversación creada</h1>
+            @endif
+        </main>
+        <footer class="sendMessage bg-blue-800 h-[10%] rounded-br-2xl">
+            <form class="flex h-full" wire:submit.prevent="enviarMensaje">
+                <input type="text" wire:model="mensajeEnviado" placeholder="Escribir mensaje"
+                    class="grow placeholder:pl-4 focus:bg-colorFondo">
+                <button type="submit" class="basis-1/6">Enviar</button>
+            </form>
+        </footer>
+    @else
+        <main>
+            <h1 class="mt-8 text-center text-2xl text-white">No hay ninguna conversación seleccionada</h1>
+        </main>
+    @endif
+
     <script>
-        document.addEventListener('livewire:init', function () {
+        document.addEventListener('livewire:init', function() {
 
 
-            Livewire.on('toggleParticipantesList', function () {
+            Livewire.on('toggleParticipantesList', function() {
                 let participantes = document.querySelector('.participantesList');
-                let flecha= document.querySelector('.flecha');
-                    participantes.classList.toggle('hidden');
-                    flecha.classList.toggle('rotate-180');  
-                let info=document.querySelector('.mostrarInfo');
+                let flecha = document.querySelector('.flecha');
+                participantes.classList.toggle('hidden');
+                flecha.classList.toggle('rotate-180');
+                let info = document.querySelector('.mostrarInfo');
                 console.log(info);
-                                
+
             });
 
         });
-
     </script>
-    
-    
+
+
 </div>
